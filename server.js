03dadbd617875b8afc4782f1733663396075167e
Rami Pinku <Prami@dalet.local>
2017-07-01 16:07:18 +0300

    heroku ready

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('hbs')
 const fs = require('fs');
 var app = express();
 
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine','hbs');
@@ -56,6 +57,6 @@ app.get('/bad', (req,res) => {
         error: "error, error, unable to compute"
     });
 });
-app.listen(3000,()=>{
-    console.log('server is up on port 3000');
+app.listen(port,()=>{
+    console.log(`server is up on port ${port}`);
 });
